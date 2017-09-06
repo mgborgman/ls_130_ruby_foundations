2b4a9c5ac3ec1280b553bfc27105e80403465810
Matt Borgman <mgborgman@gmail.com>
2017-09-06 12:54:17 -0700

    Todo List Class

--- lesson_1/TodoList.rb
+++ lesson_1/TodoList.rb
@@ -1,17 +1,3 @@
-require 'pry'
-
-class TodoList
-  attr_accessor :title
-
-  def initialize(title)
-    @title = title
-    @todos = []
-  end
-
-  # rest of class needs implementation
-
-end
-
 class Todo
   DONE_MARKER = 'X'
   UNDONE_MARKER = ' '
@@ -39,4 +25,140 @@ class Todo
   def to_s
     "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
   end
+end
+
+class TodoList
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+
+  def add(todo)
+    raise TypeError, "Can only add Todo objects" unless todo.instance_of?(Todo)
+    @todos << todo
+  end
+
+  def <<(todo)
+    raise TypeError, "Can only add Todo objects" unless todo.instance_of?(Todo)
+    @todos << todo
+  end
+
+  def size
+    @todos.size
+  end
+
+  def first
+    @todos.first
+  end
+
+  def last
+    @todos.last
+  end
+
+  def item_at(index)
+    @todos.fetch(index)
+  end
+
+  def mark_done_at(index)
+    item_at(index).done!
+  end
+
+  def mark_undone_at(index)
+    item_at(index).undone!
+  end
+
+  def shift
+    @todos.shift
+  end
+
+  def pop
+    @todos.pop
+  end
+
+  def remove_at(index)
+    @todos.delete(item_at(index))
+  end
+
+  def to_s
+    counter = 0
+    result = []
+    while counter < size
+      result << "#{item_at(counter)}"
+      counter += 1
+    end
+    result.join("\n")
+  end
+
+  def each
+    counter = 0
+
+    while counter < size
+      yield(item_at(counter))
+      counter += 1
+    end
+    self
+  end
+
+  def select
+    counter = 0
+    result = []
+    while counter < size
+      result << item_at(counter) if yield(item_at(counter))
+      counter += 1
+    end
+    list = TodoList.new(title)
+    result.each do |todo|
+      list.add(todo)
+    end
+    list
+  end
+
+  def find_by_title(title)
+    result = nil
+    select do |todo|
+      result = todo if todo.title == title
+      break unless result.nil?
+    end
+    result
+  end
+
+  def all_done
+    result = TodoList.new(title)
+    select do |todo|
+      result << todo if todo.done?
+    end
+    result
+  end
+
+  def all_not_done
+    result = TodoList.new(title)
+    select do |todo|
+      result << todo unless todo.done?
+    end
+    result
+  end
+
+  def mark_done(title)
+    result = nil
+    select do |todo|
+      result = todo if todo.title == title
+      break unless result.nil?
+    end
+    result.done! unless result.nil?
+  end
+
+  def mark_all_done
+    each do |todo|
+      todo.done!
+    end
+  end
+
+  def mark_all_undone
+    each do |todo|
+      todo.undone!
+    end
+  end
+
 end
